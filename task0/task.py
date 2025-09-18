56fd8d3b8ab011ca4b4bb8a07f214d171e75a751
daryavanina <m2203059@edu.misis.ru>
2025-09-18 18:23:13 +0300

    task0 is done

--- task0/task.py
+++ task0/task.py
@@ -1,1 +1,23 @@
+import numpy as np
 
+def main(content):
+    new_list = []
+    for item in content:
+         new_list.append(int(item[0]))
+         new_list.append(int(item[2]))
+    
+    n = max(new_list)
+
+    matrix = np.zeros((n, n), int)
+
+    for i in range (0, len(new_list), 2):
+        f_idx = new_list[i] - 1
+        s_idx = new_list[i+1] - 1
+        matrix[f_idx][s_idx] = 1
+    
+    return matrix
+
+with open("C:/progs/sis-an/misis-system-analysis/task0/task2.csv", "r") as file:
+        content = file.read().split()
+
+print(main(content))
