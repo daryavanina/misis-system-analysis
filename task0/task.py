23b6ec857791ad43dbda7312b7e6a2087f133631
daryavanina <m2203059@edu.misis.ru>
2025-09-19 16:33:55 +0300

    change to string

--- task0/task.py
+++ task0/task.py
@@ -1,21 +1,20 @@
 def main(content):
-    new_list = []
-    for item in content:
-         new_list.append(int(item[0]))
-         new_list.append(int(item[2]))
+    pairs = [item.split(',') for item in content.split('\n')]
+
+    vertexes = set([item[0] for item in pairs])#.add([item[1] for item in pairs])
+    vertexes.update([item[1] for item in pairs])
+    vertexes = sorted(vertexes)
+
+    n = len(vertexes)
+
+    matrix = [[0]*n for i in range(n)]
     
-    n = max(new_list)
+    for pair in pairs:
+        f_idx = vertexes.index(pair[0]) 
+        s_idx = vertexes.index(pair[1]) 
 
-    matrix = [[0] * n for i in range (n)]
-
-    for i in range (0, len(new_list), 2):
-        f_idx = new_list[i] - 1
-        s_idx = new_list[i+1] - 1
         matrix[f_idx][s_idx] = 1
-    
+
     return matrix
 
-with open("C:/progs/sis-an/misis-system-analysis/task0/task2.csv", "r") as file:
-        content = file.read().split()
-
-print(main(content))
+print(main("1,2\n1,3\n3,4\n3,5"))
